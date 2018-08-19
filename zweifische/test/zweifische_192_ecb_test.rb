6f7d81878b8886fc7b349051048d86024e4e3258
Nurahmadie <nurahmadie@gmail.com>
2018-08-19 14:05:14 +0700

    ECB encrypt and decrypt tests for zweifische

---
+++ zweifische/test/zweifische_192_ecb_test.rb
@@ -0,0 +1,28 @@
+require "test_helper"
+require "known_answers"
+
+class Zweifische192ECBTest < Minitest::Test
+  def ECB_encrypt_test(idx)
+    i = ENC_TABLE_192[idx]
+    tf = Zweifische::Cipher192ecb.new(hexify i[:KEY])
+    assert_equal tf.encrypt(hexify(i[:PT])), hexify(i[:CT])
+  end
+
+  def ECB_decrypt_test(idx)
+    i = ENC_TABLE_192[idx]
+    tf = Zweifische::Cipher192ecb.new(hexify i[:KEY])
+    assert_equal tf.decrypt(hexify(i[:CT])), hexify(i[:PT])
+  end
+
+  (1..10).each do |i|
+    class_eval <<-EODEF
+    def test_ECB_I#{i}_encrypt
+      ECB_encrypt_test(#{i - 1})
+    end
+
+    def test_ECB_I#{i}_decrypt
+      ECB_decrypt_test(#{i - 1})
+    end
+    EODEF
+  end
+end
